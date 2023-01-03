42e92fa22348efa54fb93d206748da9c84fec448
actuallyzefe <zefek10@gmail.com>
2023-01-03 23:07:15 +0300

    country

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const axios = require('axios');
 const { JSDOM } = require('jsdom');
 const { html } = require('parse5');
 
-const getProductUrl = (product_id) =>
-  `https://www.amazon.com/gp/product/ajax/ref=dp_aod_NEW_mbc?asin=${product_id}&m=&smid=&sourcecustomerorglistid=&sourcecustomerorglistitemid=&sr=8-6&pc=dp&experienceId=aodAjaxMain`;
+const getProductUrl = (country, product_id) =>
+  `https://www.amazon.${country}/gp/product/ajax/ref=dp_aod_NEW_mbc?asin=${product_id}&m=&smid=&sourcecustomerorglistid=&sourcecustomerorglistitemid=&sr=8-6&pc=dp&experienceId=aodAjaxMain`;
 
-async function getPrices(product_id) {
-  const productUrl = getProductUrl(product_id);
+async function getPrices(country, product_id) {
+  const productUrl = getProductUrl(country, product_id);
   const { data: html } = await axios.get(productUrl, {
     headers: {
       Accept:
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      Host: 'www.amazon.com',
+      Host: `www.amazon.${country}`,
       'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
       pragma: 'no - cache',
@@ -58,4 +58,4 @@ async function getPrices(product_id) {
   console.log(result);
 }
 
-getPrices('B09FJDTX41');
+getPrices('ca', 'B01723DLYE');
